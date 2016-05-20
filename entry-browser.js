005ed1bae9578df3580ee1a1c3e496c95b0e3823
gebrits <gbrits@gmail.com>
2016-05-20 22:55:23 +0200

    retry

--- entry-browser.js
+++ entry-browser.js
@@ -1,3 +1,23 @@
+wnidow.Response = function Response(req, options) {
+  console.log("ASDASD");
+  options = options || {};
+  this.req = req;
+  this.xhr = this.req.xhr;
+  // responseText is accessible only if responseType is '' or 'text' and on older browsers
+  this.text = ((this.req.method != 'HEAD' && (this.xhr.responseType === '' || this.xhr.responseType === 'text')) || typeof this.xhr.responseType === 'undefined') ? this.xhr.responseText : null;
+  this.statusText = this.req.xhr.statusText;
+  this.setStatusProperties(this.xhr.status);
+  this.header = this.headers = parseHeader(this.xhr.getAllResponseHeaders());
+  // getAllResponseHeaders sometimes falsely returns "" for CORS requests, but
+  // getResponseHeader still works. so we get content-type even if getting
+  // other headers fails.
+  this.header['content-type'] = this.xhr.getResponseHeader('content-type');
+  this.setHeaderProperties(this.header);
+  this.body = this.req.method != 'HEAD' ? this.parseBody(this.text ? this.text : this.xhr.response) : null;
+};
+
+
+
 var Xray = require("./index");
 // var cheerio = require('cheerio');
 
